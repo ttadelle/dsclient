e595d35b244c9a794d1cb2cbf8413db6c0e621ab
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-26 20:26:48 -0300

    Correcao Resource e Service

--- backend/src/main/java/com/example/tadelle/dsclient/resource/ClientResource.java
+++ backend/src/main/java/com/example/tadelle/dsclient/resource/ClientResource.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class ClientResource {
 	@Autowired
 	private ClientService clientService;
 	
-	@Transactional(readOnly = true)
+	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 		@RequestParam(value = "page",defaultValue = "0") Integer page,
 		@RequestParam(value = "linesPerPage",defaultValue = "12") Integer linesPerPage,
--- backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
+++ backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.tadelle.dsclient.dto.ClientDTO;
 import com.example.tadelle.dsclient.entities.Client;
@@ -15,6 +16,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository clientRepository;
 	
+	@Transactional(readOnly=true)
 	public Page<ClientDTO> findAll(PageRequest pageRequest) {
 		Page<Client> pageList = clientRepository.findAll(pageRequest);
 		return pageList.map(x -> (new ClientDTO(x)));
