ab3f585f808d0e1ca7480a495d5c40924e6c82cb
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-27 10:18:55 -0300

    Exceptions e GET id

--- backend/src/main/java/com/example/tadelle/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/example/tadelle/dsclient/resources/ClientResource.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,12 @@ public class ClientResource {
 		
 		Page<ClientDTO> pageList = clientService.findAll(pageRequest);
 		return ResponseEntity.ok().body(pageList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity <ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO clientDto = clientService.findById(id);
+		return ResponseEntity.ok().body(clientDto);
 	}
 
 }
--- backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
+++ backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.example.tadelle.dsclient.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.tadelle.dsclient.dto.ClientDTO;
 import com.example.tadelle.dsclient.entities.Client;
 import com.example.tadelle.dsclient.repositories.ClientRepository;
+import com.example.tadelle.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +23,13 @@ public class ClientService {
 	public Page<ClientDTO> findAll(PageRequest pageRequest) {
 		Page<Client> pageList = clientRepository.findAll(pageRequest);
 		return pageList.map(x -> (new ClientDTO(x)));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> optional = clientRepository.findById(id);
+		Client entity = optional.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+		return new ClientDTO(entity);
 	}
 
 }
