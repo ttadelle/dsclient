1e0cef9cf2914c1fbdf33efab687e7e794710f0e
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-27 13:57:27 -0300

    DELETE created

--- backend/src/main/java/com/example/tadelle/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/example/tadelle/dsclient/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,12 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
 		dto = clientService.update(id, dto);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value= "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		clientService.delete(id);
+		return ResponseEntity.noContent().build();		
 	}
 	
 }
--- backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
+++ backend/src/main/java/com/example/tadelle/dsclient/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.tadelle.dsclient.dto.ClientDTO;
 import com.example.tadelle.dsclient.entities.Client;
 import com.example.tadelle.dsclient.repositories.ClientRepository;
+import com.example.tadelle.dsclient.services.exceptions.DatabaseException;
 import com.example.tadelle.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,5 +64,18 @@ public class ClientService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
-	}	
+	}
+	
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
+	}
 }
